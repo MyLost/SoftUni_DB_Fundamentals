cbb9ec06c75c8a031a51586a57d6680911c03252
darthReader <ddimitrov10@gmail.com>
2022-09-08 09:44:17 +0300

    database exercise

--- Exercises: Subqueries and JOINs/*Continents and Currencies.sql
+++ Exercises: Subqueries and JOINs/*Continents and Currencies.sql
@@ -1,1 +1,12 @@
-select * from continents;
+SELECT continent_code, currency_code, count(currency_code) AS currency_usage
+FROM countries c1
+GROUP BY continent_code, currency_code
+HAVING currency_usage = (
+  SELECT count(currency_code) AS count
+    FROM countries c2
+    WHERE c2.continent_code = c1.continent_code
+    GROUP BY c2.currency_code
+    ORDER BY count DESC
+    LIMIT 1
+  ) AND currency_usage > 1
+ORDER BY continent_code , currency_code;
