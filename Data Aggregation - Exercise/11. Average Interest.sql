51472bee73b7126c78c0e1bc494b34cd577de192
darthReader <ddimitrov10@gmail.com>
2022-09-21 08:59:56 +0300

    database exercise

---
+++ Data Aggregation - Exercise/11. Average Interest.sql
@@ -0,0 +1,6 @@
+select * from wizzard_deposits;
+
+select deposit_group, is_deposit_expired, avg(deposit_interest) from wizzard_deposits
+where deposit_start_date > '1985-01-01'
+group by deposit_group, is_deposit_expired
+order by deposit_group desc, is_deposit_expired asc;
