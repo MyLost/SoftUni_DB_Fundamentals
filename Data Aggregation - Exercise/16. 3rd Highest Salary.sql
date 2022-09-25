f92f135fa94328bd52ad775c438a8630be947704
darthReader <ddimitrov10@gmail.com>
2022-09-25 10:50:51 +0300

    database exercise

---
+++ Data Aggregation - Exercise/16. 3rd Highest Salary.sql
@@ -0,0 +1,11 @@
+select department_id,
+    (select distinct salary from employees as e2
+     where e1.department_id = e2.department_id
+     order by salary desc limit 2,1) as third_highest_salary
+from employees e1
+group by department_id
+having third_highest_salary is not null
+order by department_id
+
+
+
